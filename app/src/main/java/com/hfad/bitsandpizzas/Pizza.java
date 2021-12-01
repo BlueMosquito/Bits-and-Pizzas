4a987f3aad521aeeaea4adfdcc19c425873320b2
BlueMosquito <61717325+BlueMosquito@users.noreply.github.com>
2021-12-01 13:23:26 +0100

    pizza

--- app/src/main/java/com/hfad/bitsandpizzas/Pizza.java
+++ app/src/main/java/com/hfad/bitsandpizzas/Pizza.java
@@ -3,7 +3,6 @@ package com.hfad.bitsandpizzas;
 public class Pizza {
     private String name;
     private int imageResourceId;
-
     public static final Pizza[] pizzas = {
             new Pizza("Diavolo", R.drawable.diavolo),
             new Pizza("Funghi", R.drawable.funghi)
