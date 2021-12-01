66e951be033ce0a4500df2bf52a38f9c48167a4c
BlueMosquito <61717325+BlueMosquito@users.noreply.github.com>
2021-12-01 17:23:52 +0100

    done

--- app/src/main/java/com/hfad/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/com/hfad/bitsandpizzas/CaptionedImagesAdapter.java
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +16,11 @@ class CaptionedImagesAdapter extends
 
     private String[] captions;
     private int[] imageIds;
+    private Listener listener;
+
+    interface Listener {
+        void onClick(int position);
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private CardView cardView;
@@ -33,6 +39,10 @@ class CaptionedImagesAdapter extends
     @Override
     public int getItemCount() {
         return captions.length;
+    }
+
+    public void setListener(Listener listener){
+        this.listener = listener;
     }
 
     @NonNull
@@ -54,5 +64,14 @@ class CaptionedImagesAdapter extends
         imageView.setContentDescription(captions[position]);
         TextView textView = (TextView)cardView.findViewById(R.id.info_text);
         textView.setText(captions[position]);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(listener != null){
+                    listener.onClick(position);
+                }
+            }
+        });
     }
 }
